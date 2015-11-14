8c8c2d04fb292728298e1dd5ffc1581853bf50b3
Prabesh Manandhar <manandharprabesh@gmail.com>
2015-11-13 20:41:13 -0600

    Added create user and edit user views

--- src/main/java/map/controller/AdminController.java
+++ src/main/java/map/controller/AdminController.java
@@ -1,8 +1,16 @@
 package map.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import map.domain.User;
 
 @Controller
 public class AdminController {
@@ -21,5 +29,23 @@ public class AdminController {
 	public String listMaps(Model model){
 		//TODO get list of maps
 		return "admin/maps";
+	}
+	@RequestMapping(value = "/admin/createUser", method=RequestMethod.GET)
+	public String createUser(@ModelAttribute("User")User user){
+		return "admin/createUser";
+	}
+	
+	@RequestMapping(value="/admin/createUser", method=RequestMethod.POST)
+	public String addUser(Model model,@Valid @ModelAttribute("User")User user, BindingResult result){
+		if(result.hasErrors()){
+			//Load form
+		}
+		return "admin/createUser";
+	}
+	
+	@RequestMapping("/admin/user/edit/{id}")
+	public String editUser(@PathVariable("id")String id, Model model){
+		//TODO get user from db
+		return "admin/edituser";
 	}
 }
--- src/main/webapp/WEB-INF/jsp/admin/users.jsp
+++ src/main/webapp/WEB-INF/jsp/admin/users.jsp
@@ -1,5 +1,5 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
-    pageEncoding="ISO-8859-1"%>
+	pageEncoding="ISO-8859-1"%>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
@@ -7,6 +7,17 @@
 <title>User List</title>
 </head>
 <body>
-<h1>Users List</h1>
+	<h1>Users List</h1>
+	<a href="admin/createUser">Create a user</a>
+	 <table>
+            <c:forEach var="user" items="${users}">
+                <tr>
+                    <td>${user.username}</td>                   
+                    <sec:authorize access="hasRole('ROLE_ADMIN')" >
+                        <td><a href="admin/user/edit/${user.id}">edit</a></td>
+                    </sec:authorize>
+                </tr>
+            </c:forEach>
+        </table>
 </body>
 </html>
